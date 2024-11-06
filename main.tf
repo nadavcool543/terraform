fa8e5c30acf20a32f7ba841c00ea6863c8935a15
nadavcool543 <nadavcool543@gmail.com>
2024-11-06 12:54:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,31 +10,16 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"  
+  profile = "default"
 }
 
 resource "aws_key_pair" "netflix_app_key" {
-  key_name   = "netflix-app-key"  
-  public_key = file("C:/Users/nadav/OneDrive/Desktop/mykey.pub")  
-}
-
-resource "aws_instance" "netflix_app" {
-  ami           = "ami-06b21ccaeff8cd686"
-  instance_type = "t2.nano"
-  key_name      = aws_key_pair.netflix_app_key.key_name
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name       = "Netflix App"
-    Environment = "Development"
-   
-  }
-
-  security_groups = [aws_security_group.netflix_app_sg.name]
+  key_name   = "netflix-app-key"
+  public_key = file("C:/Users/nadav/OneDrive/Desktop/mykey.pub")
 }
 
 resource "aws_security_group" "netflix_app_sg" {
-  name        = "netflix-app-sg"   
+  name        = "netflix-app-sg"
   description = "Allow SSH and HTTP traffic"
 
   ingress {
@@ -59,16 +44,43 @@ resource "aws_security_group" "netflix_app_sg" {
   }
 }
 
+
+resource "aws_s3_bucket" "netflix_app_bucket" {
+  bucket = "netflix-app-nadav241198"  
+  acl    = "private"
+
+  tags = {
+    Name = "Netflix App Bucket"
+  }
+}
+
+resource "aws_instance" "netflix_app" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.nano"
+  key_name      = aws_key_pair.netflix_app_key.key_name
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name        = "Netflix App"
+    Environment = "Development"
+  }
+
+  security_groups = [aws_security_group.netflix_app_sg.name]
+
+  depends_on = [aws_s3_bucket.netflix_app_bucket]
+}
+
 resource "aws_ebs_volume" "netflix_app_volume" {
-  availability_zone = "us-east-1a"  
-  size              = 5              
+  availability_zone = "us-east-1a"
+  size              = 5
+
   tags = {
     Name = "Netflix App Volume"
   }
 }
 
 resource "aws_volume_attachment" "netflix_app_volume_attachment" {
-  device_name = "/dev/sdf"  
+  device_name = "/dev/sdf"
   volume_id   = aws_ebs_volume.netflix_app_volume.id
   instance_id = aws_instance.netflix_app.id
 }
