1ca64e86ccfac512f437f1724f8662f3bd457d3e
nadavcool543 <nadavcool543@gmail.com>
2024-11-06 12:18:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ terraform {
       version = ">=5.55"
     }
   }
-
   required_version = ">= 1.7.0"
 }
 
@@ -19,7 +18,36 @@ resource "aws_instance" "netflix_app" {
   instance_type = "t2.nano"
 
   tags = {
-    Name = "Netflix App"
-    Enviorment = "Development"
+    Name       = "Netflix App"
+    Environment = "Development"
+  }
+
+  security_groups = [aws_security_group.nadav-netflix-app-sg.name]
+}
+
+resource "aws_security_group" "nadav-netflix_app_sg" {
+  name        = "nadav-netflix-app-sg"   # change <your-name> accordingly
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
